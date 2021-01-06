28c948dff514f206044984c7fae462aaba09ea9e
SanjayMarreddi <sanjay.mareddi@gmail.com>
2021-01-06 19:21:44 +0530

    Modified to account for the case when no face is detected

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-# First let us import required Functions from FLASK
-from flask import Flask, render_template, Response, request, redirect, flash, url_for
-from werkzeug.utils import secure_filename
-
-# From the module "camera" let us import the VideoCamera Class
-from camera import VideoCamera
-
-# From the module "Graphical_Visualisation" let us import the Emotion_Analysis Function
-from Graphical_Visualisation import Emotion_Analysis
-
-# Import the other important Libraries
+# Let us import the Libraries required.
 import os
 import cv2
 import urllib
 import numpy as np
+from werkzeug.utils import secure_filename
 from urllib.request import Request, urlopen
+from flask import Flask, render_template, Response, request, redirect, flash, url_for
 
+# Importing the required Classes/Functions from Modules defined.
+from camera import VideoCamera
+from Graphical_Visualisation import Emotion_Analysis
 
 # Let us Instantiate the app
 app = Flask(__name__)
 
-# Refreshing the Cache
+###################################################################################
+# We define some global parameters so that its easier for us to tweak when required.
+
+# When serving files, we set the cache control max age to zero number of seconds
+# for refreshing the Cache
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-###############################################################################
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+###################################################################################
 # Some Utility Functions
+
+# Flask provides native support for streaming responses through the use of generator
+# functions. A generator is a special function that can be interrupted and resumed.
 
 
 def gen(camera):
-    # Some Useful function for Video Feed
-
+    "" "Helps in Passing frames from Web Camera to server"""
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
@@ -36,67 +41,68 @@ def gen(camera):
 
 
 def allowed_file(filename):
-    # Useful function for fetching Image form URL
-
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-###############################################################################
-
-# We define some global parameters so that its easier for us to tweak when required.
+    """ Checks the file format when file is uploaded"""
+    return ('.' in filename and
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
 
 
-UPLOAD_FOLDER = 'static'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+###################################################################################
 
-
-###############################################################################
-
-# Defining the route for Home Page
 @app.route('/')
 def Start():
+    """ Renders the Home Page """
+
     return render_template('Start.html')
 
 
-# Defining the route for Taking Video Feed
 @app.route('/video_feed')
 def video_feed():
+    """ A route that returns a streamed response needs to return a Response object
+    that is initialized with the generator function."""
+
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-# Defining the route for Capturing an Real Time Image from WebCam Video Stream.
 @app.route('/RealTime', methods=['POST'])
 def RealTime():
-    """Video streaming home page."""
+    """ Video streaming (Real Time Image from WebCam Video) home page."""
+
     return render_template('RealTime.html')
 
 
-# Defining the route for Taking Image from WebCam and saving it
 @app.route('/takeimage', methods=['POST'])
 def takeimage():
+    """ Captures Images from WebCam, saves them, does Emotion Analysis & renders. """
+
     v = VideoCamera()
     _, frame = v.video.read()
     save_to = "static/"
     cv2.imwrite(save_to + "capture" + ".jpg", frame)
+
     result = Emotion_Analysis("capture.jpg")
+
+    # When Classifier could not detect any Face.
+    if len(result) == 1:
+        return render_template('NoDetection.html', orig=result[0])
+
     return render_template('Visual.html', orig=result[0], pred=result[1], bar=result[2])
 
 
-# Defining the route for Manual Uploading of Images
 @app.route('/ManualUpload', methods=['POST'])
 def ManualUpload():
-    """Video streaming home page."""
+    """ Manual Uploading of Images via URL or Upload """
+
     return render_template('ManualUpload.html')
 
 
-# Defining the route for Manual Uploading of Images via UPLOAD from system
 @app.route('/uploadimage', methods=['POST'])
 def uploadimage():
+    """ Loads Image from System, does Emotion Analysis & renders."""
+
     if request.method == 'POST':
 
-        # check if the post request has the file part
+        # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -111,32 +117,43 @@ def uploadimage():
 
         # If user uploads the correct Image File
         if file and allowed_file(file.filename):
+
+            # Pass it a filename and it will return a secure version of it.
+            # The filename returned is an ASCII only string for maximum portability.
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            # Start the Facial Expression Recognition
             result = Emotion_Analysis(filename)
-            return render_template('visual.html', orig=result[0], pred=result[1], bar=result[2])
+
+            # When Classifier could not detect any Face.
+            if len(result) == 1:
+                return render_template('NoDetection.html', orig=result[0])
+
+            return render_template('Visual.html', orig=result[0], pred=result[1], bar=result[2])
 
 
-# Defining the route for Manual Uploading of Images via providing URL of Image
 @app.route('/imageurl', methods=['POST'])
 def imageurl():
+    """ Fetches Image from URL Provided, does Emotion Analysis & renders."""
 
     # Fetch the Image from the Provided URL
     url = request.form['url']
     req = Request(url,
                   headers={'User-Agent': 'Mozilla/5.0'})
 
-    # Encoding and Saving it to the static Folder
+    # Reading, Encoding and Saving it to the static Folder
     webpage = urlopen(req).read()
     arr = np.asarray(bytearray(webpage), dtype=np.uint8)
     img = cv2.imdecode(arr, -1)
     save_to = "static/"
     cv2.imwrite(save_to + "url.jpg", img)
 
-    # Start the Facial Expression Recognition
     result = Emotion_Analysis("url.jpg")
+
+    # When Classifier could not detect any Face.
+    if len(result) == 1:
+        return render_template('NoDetection.html', orig=result[0])
+
     return render_template('Visual.html', orig=result[0], pred=result[1], bar=result[2])
 
 
