3a29d756ae0a45394364993ed5b991aac3cf15e3
SanjayMarreddi <sanjay.mareddi@gmail.com>
2021-01-06 19:23:43 +0530

    Enhanced the Content presentation in the code

--- camera.py
+++ camera.py
@@ -2,69 +2,82 @@
 import cv2
 import numpy as np
 
-# From Module named "model", Let us import the FacialExpressionModel class.
 from model import FacialExpressionModel
 
 # Creating an instance of the class with the parameters as model and its weights.
 model = FacialExpressionModel("model.json", "model_weights.h5")
+
+# Loading the classifier from the file.
 facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Let us define a Class for taking the Real time Video and Predicting the Emotion using our model
 
 class VideoCamera(object):
-    
-    # Whenever we create an instance of class, video variable is created and its starts taking the Video
-    # using System's Default Web Cam
+
+    """ Takes the Real time Video, Predicts the Emotion using pre-trained model. """
+
     def __init__(self):
         self.video = cv2.VideoCapture(0)
 
     def __del__(self):
         self.video.release()
 
-    # Let us define a function that returns camera frames along with bounding boxes and predictions
     def get_frame(self):
+        """It returns camera frames along with bounding boxes and predictions"""
 
         # Reading the Video and grasping the Frames
-        _, fr = self.video.read()
-                    
-        # Converting the Color image to Gray Scale 
-        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        _, frame = self.video.read()
+
+        # Converting the Color image to Gray Scale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Image size is reduced by 30% at each image scale.
+        scaleFactor = 1.3
+
+        # 5 neighbors should be present for each rectangle to be retained.
+        minNeighbors = 5
 
         # Detect the Faces in the given Image and store it in faces.
-        faces = facec.detectMultiScale(gray_fr, 1.3, 5)
-        
+        faces = facec.detectMultiScale(gray_frame, scaleFactor, minNeighbors)
+
         # Iterating through all the faces detected
         for (x, y, w, h) in faces:
-            
-            # Taking the Face part in the Image
-            fc = gray_fr[y:y+h, x:x+w]
-            
-            # Let us resize the Image and store it as Region of Interest(roi) 
-            roi = cv2.resize(fc, (48, 48))
 
-            # Let us make the Prediction of Emotion present in the Image
-            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+            # Taking the Face part in the Image as Region of Interest.
+            roi = gray_frame[y:y+h, x:x+w]
 
-            # Let us define text and its Font style that is to be written on Image
-            sym={"Happy":":)","Sad":":}","Surprise":"!!","Angry":"?","Disgust":"#","Neutral":".","Fear":"~"}
-            text=  str(pred) +  sym[str(pred)] 
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            
+            # Let us resize the Image accordingly to use pretrained model.
+            roi = cv2.resize(roi, (48, 48))
+
+            # Let us make the Prediction of Emotion present in the Image.
+            prediction = model.predict_emotion(
+                roi[np.newaxis, :, :, np.newaxis])
+
+            # Custom Symbols to print with text of emotion.
+            Symbols = {"Happy": ":)", "Sad": ":}", "Surprise": "!!",
+                       "Angry": "?", "Disgust": "#", "Neutral": ".", "Fear": "~"}
+
+            # Defining the Parameters for putting Text on Image
+            Text = str(prediction) + Symbols[str(prediction)]
+            Text_Color = (180, 105, 255)
+
+            Thickness = 2
+            Font_Scale = 1
+            Font_Type = cv2.FONT_HERSHEY_SIMPLEX
+
             # Inserting the Text on Image
-            cv2.putText(fr,text, (x, y), font, 1, (180,105,255), 2)
-            
+            cv2.putText(frame, Text, (x, y), Font_Type,
+                        Font_Scale, Text_Color, Thickness)
+
             # Finding the Coordinates and Radius of Circle
-            xc = (x + x+w)/2
-            yc = (y + y+h)/2
-            radius = w/2 # or h/2
+            xc = int((x + x+w)/2)
+            yc = int((y + y+h)/2)
+            radius = int(w/2)
 
             # Drawing the Circle on the Image
-            cv2.circle(fr, (int(xc),int(yc)), int(radius), (0,255,0), 2)
-           
+            cv2.circle(frame, (xc, yc), radius, (0, 255, 0), Thickness)
+
         # Encoding the Image into a memory buffer
-        _, jpeg = cv2.imencode('.jpg', fr)
+        _, jpeg = cv2.imencode('.jpg', frame)
 
-        # Returning the Image
+        # Returning the image as a bytes object
         return jpeg.tobytes()
-
-
