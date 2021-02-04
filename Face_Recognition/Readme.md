b6cbc9815772bdf89779c04e74e6ba3c59c46b8c
Amit kumar mishra <akm43300@gmail.com>
2021-02-04 16:59:53 +0530

    Updated changes

--- Face_Recognition/Readme.md
+++
@@ -1,5 +0,0 @@
-## Face-Recognition
-
-This project is trained to identify the avengers like hulk, thor, captain_america and tony.
-
-The project asks the user for input and in the given photo if the above mentioned images are present then it represents the face with a rectangule with the name on it.
--- Facial_Recognition_Models/Using_Standard_Libraries/Bharath_C_S/Basic_Avengers_Recognition/script.py
+++ Facial_Recognition_Models/Using_Standard_Libraries/Bharath_C_S/Basic_Avengers_Recognition/script.py
@@ -10,6 +10,7 @@ loadimg = askopenfilename()
 targetimg = fr.load_image_file(loadimg)
 targetencode = fr.face_encodings(targetimg)
 
+##Training the model by providing the images
 def encodeface(images):
     list_people = []
 
@@ -21,10 +22,11 @@ def encodeface(images):
 
     return list_people
 
+##This function takes the image provided by the user and compares with the trained models
 def find_target():
     face_location = fr.face_locations(targetimg)
 
-    for people in encodeface('data_img/'):
+    for people in encodeface("./data_img"):
         encodedface = people[0]
         files = people[1]
 
@@ -44,13 +46,14 @@ def find_target():
 
 
 
-
+## Here the rectangles around the faces is created
 def create_frame(location, label):
     top, right, bottom, left = location
     cv.rectangle(targetimg, (left,top), (right,bottom), (255, 0, 0), 2)
     cv.rectangle(targetimg, (left,bottom + 20), (right,bottom), (255, 0, 0), cv.FILLED)
     cv.putText(targetimg, label, (left + 3, bottom + 14), cv.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255), 1)
 
+## It shows the recognized image with label
 def renderimg():
     rgb_img = cv.cvtColor(targetimg, cv.COLOR_BGR2RGB)
     cv.imshow('Face Recognition', rgb_img)
